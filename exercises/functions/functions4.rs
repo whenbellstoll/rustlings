7fa53008e5a19864329e475326e35218ae2d3c1b
Kent Reese <kentreese64@gmail.com>
2020-05-16 23:18:56 -0400

    Functions Exercises

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -11,14 +11,14 @@ fn main() {
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: i32) -> i32 {
     if is_even(price) {
-        price - 10
+        return price - 10
     } else {
-        price - 3
+        return price - 3
     }
 }
 
 fn is_even(num: i32) -> bool {
-    num % 2 == 0
+    return num % 2 == 0;
 }
